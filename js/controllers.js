626fb7e40e6355515884fb1b7a4a951647b720f6
zvika Badalov <zvika@badalov.us>
2017-07-14 11:41:25 -0400

    fixed the footer controller to be global using the js\controller.js file

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,11 @@
-// controller.js
+// controller.js global controllers should be placed here
+angular
+.module('app')
+.controller('footerCtrl', footerCtrl);
+
+footerCtrl.$inject = ['$scope'];
+function footerCtrl($scope){
+  $scope.date = new Date();
+  $scope.businessName = 'Replace Me with Business Name';
+  $scope.businessUrl = 'http://www.acme.com'
+}
